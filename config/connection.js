f4e2071684bd68f34e6e09ffd79f8626a4c46a46
Iryna <iryna.shulim@gmail.com>
2021-08-05 18:39:22 -0500

    category model, defining columns for id and category_name

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
-
+// import the Sequelize constructor from the library
 const Sequelize = require('sequelize');
-
+// create connection to our database
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
