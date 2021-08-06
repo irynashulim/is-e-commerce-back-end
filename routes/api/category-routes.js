3dba9761879fb5f0c2b5c9a2b4bf9e3587993d02
Iryna <iryna.shulim@gmail.com>
2021-08-06 14:58:40 -0500

    adding 'include' to category get routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -5,7 +5,15 @@ const { Category, Product } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all categories
-  Category.findAll()
+  Category.findAll({
+    attributes: [ "id", "category_name"],
+    include: [
+      {
+        model: Product,
+        attributes: ['product_name']
+      }
+    ]
+  })
   .then(dbCategoryData => res.json(dbCategoryData))
   .catch(err => {
     console.log(err);
@@ -20,7 +28,14 @@ router.get('/:id', (req, res) => {
   Category.findOne({
     where: {
       id: req.params.id
-    }
+    },
+    attributes: ["id", "category_name"],
+    include: [
+      {
+        model: Product,
+        attributes: ["product_name"]
+      }
+    ]
   })
   .then (dbCategoryData => {
     if(dbCategoryData) {
