112ae8a8b0d8021c2b126171a03187c566a48a9a
Iryna <iryna.shulim@gmail.com>
2021-08-07 14:35:13 -0500

    fixed post route for categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -55,6 +55,11 @@ router.post('/', (req, res) => {
   Category.create({
     category_name: req.body.category_name
   })
+.then(dbCategoryData => res.json(dbCategoryData))
+.catch(err => {
+  console.log(err);
+  res.status(500).json(err);
+});
 });
 
 router.put('/:id', (req, res) => {
