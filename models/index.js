71ef9f723788d31b7652ab9371f38bb58735408f
Iryna <iryna.shulim@gmail.com>
2021-08-08 22:08:02 -0500

    Adding associations to Product and ProductTag to fix delete route for categories

--- models/index.js
+++ models/index.js
@@ -15,12 +15,28 @@ Category.hasMany(Product, {
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
-  foreignKey: 'product_id'
+  foreignKey: "product_id"
 });
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag,
-  foreignKey: 'tag_id'
+  foreignKey: "tag_id"
+});
+
+Product.hasMany(ProductTag, {
+  foreignKey: "product_id"
+});
+
+ProductTag.belongsTo(Product, {
+  foreignKey: "product_id"
+});
+
+ProductTag.belongsTo(Tag, {
+  foreignKey: "tag_id"
+});
+
+Tag.hasMany(ProductTag, {
+  foreignKey: "tag_id"
 });
 
 
